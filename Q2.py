848dcc6f482acaa90e09d6ec5347550151ce3e20
Henry Dai <csdai0324@gmail.com>
2017-09-15 15:27:41 +0800

    nothing

---
+++ Q2.py
@@ -0,0 +1,17 @@
+import sys
+from PIL import Image
+
+def half_pixel_value(img):
+
+	width, height = img.size
+	pixels = list(img.getdata())
+	new_pixels = []
+	for pixel_value in pixels:
+		new_pixels.append(tuple([int(value/2) for value in list(pixel_value)]))
+	new_img = Image.new('RGB', (width, height))
+	new_img.putdata(new_pixels)
+	new_img.save('Q2.png')
+
+if __name__ == '__main__':
+
+	new_img = half_pixel_value(Image.open(sys.argv[1]))
